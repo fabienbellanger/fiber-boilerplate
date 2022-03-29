f92f81de6eb789131a52f51e5e794188ca328c9d
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-03-29 18:22:54 +0200

    Fix drill

--- handlers/web/web.go
+++ handlers/web/web.go
@@ -8,7 +8,6 @@ import (
 // HealthCheck returns status code 200.
 func HealthCheck(logger *zap.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		logger.Debug("Health check OK")
 		return c.SendStatus(fiber.StatusOK)
 	}
 }

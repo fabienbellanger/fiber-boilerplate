073888a84e997d2c98142cca8334056f88f06121
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-11-25 10:49:15 +0100

    Fix entities

--- entities/task.go
+++ entities/task.go
@@ -10,7 +10,7 @@ import (
 type Task struct {
 	ID          string         `json:"id" xml:"id" form:"id" gorm:"primaryKey"`
 	Name        string         `json:"name" xml:"name" form:"not null;name" gorm:"size:127" validate:"required,min=3,max=127"`
-	Description string         `json:"description" xml:"description" form:"description" gorm:"size=127"`
+	Description string         `json:"description" xml:"description" form:"description" gorm:"size:127"`
 	CreatedAt   time.Time      `json:"created_at" xml:"created_at" form:"created_at" gorm:"not null;autoCreateTime"`
 	UpdatedAt   time.Time      `json:"updated_at" xml:"updated_at" form:"updated_at" gorm:"not null;autoUpdateTime"`
 	DeletedAt   gorm.DeletedAt `json:"-" xml:"-" form:"deleted_at" gorm:"index"`
--- entities/user.go
+++ entities/user.go
@@ -10,14 +10,15 @@ import (
 
 // User represents a user in database.
 type User struct {
-	ID        string         `json:"id" xml:"id" form:"id" gorm:"primaryKey" validate:"required,uuid"`
-	Username  string         `json:"username" xml:"username" form:"username" gorm:"not null;unique;size:127" validate:"required,email"`
-	Password  string         `json:"-" xml:"-" form:"password" gorm:"not null;index;size=128" validate:"required,min=8"` // SHA512
-	Lastname  string         `json:"lastname" xml:"lastname" form:"lastname" gorm:"size=63" validate:"required"`
-	Firstname string         `json:"firstname" xml:"firstname" form:"firstname" gorm:"size=63" validate:"required"`
-	CreatedAt time.Time      `json:"created_at" xml:"created_at" form:"created_at" gorm:"not null;autoCreateTime"`
-	UpdatedAt time.Time      `json:"updated_at" xml:"updated_at" form:"updated_at" gorm:"not null;autoUpdateTime"`
-	DeletedAt gorm.DeletedAt `json:"-" xml:"-" form:"deleted_at" gorm:"index"`
+	ID            string         `json:"id" xml:"id" form:"id" gorm:"primaryKey" validate:"required,uuid"`
+	Username      string         `json:"username" xml:"username" form:"username" gorm:"not null;unique;size:127" validate:"required,email"`
+	Password      string         `json:"-" xml:"-" form:"password" gorm:"not null;index;size:128" validate:"required,min=8"` // SHA512
+	Lastname      string         `json:"lastname" xml:"lastname" form:"lastname" gorm:"size:63" validate:"required"`
+	Firstname     string         `json:"firstname" xml:"firstname" form:"firstname" gorm:"size:63" validate:"required"`
+	CreatedAt     time.Time      `json:"created_at" xml:"created_at" form:"created_at" gorm:"not null;autoCreateTime"`
+	UpdatedAt     time.Time      `json:"updated_at" xml:"updated_at" form:"updated_at" gorm:"not null;autoUpdateTime"`
+	DeletedAt     gorm.DeletedAt `json:"-" xml:"-" form:"deleted_at" gorm:"index"`
+	PasswordReset PasswordResets `json:"-" xml:"-" form:"-" gorm:"constraint:OnDelete:CASCADE"`
 }
 
 // UserForm is used to create or update a user.
